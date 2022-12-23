96b63fc6276e6980f4e3c4c85424cff2ec232285
Maria Walzl <maria.walzl@mayflower.de>
2022-12-23 14:19:34 +0100

    add recipes empty screen

--- src/screens/Recipes.tsx
+++ src/screens/Recipes.tsx
@@ -75,6 +75,7 @@ export default function Recipes({ navigation }: RootTabScreenProps<'Recipes'>) {
 
   useEffect(() => {
     // FAKE DATA
+    // AsyncStorage.removeItem('@recipeList');
     AsyncStorage.setItem('@recipeList', JSON.stringify(DATA));
     // FAKE DATA
 
@@ -98,6 +99,16 @@ export default function Recipes({ navigation }: RootTabScreenProps<'Recipes'>) {
     container: {
       flex: 1,
       paddingTop: StatusBar.currentHeight,
+    },
+    emptyScreen: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100%'
+    },
+    emptyScreenText: {
+      color: Colors[scheme].placeholder,
+      fontSize: 16
     },
     item: {
       paddingVertical: 0,
@@ -131,10 +142,38 @@ export default function Recipes({ navigation }: RootTabScreenProps<'Recipes'>) {
       fontSize: 14,
       color: Colors[scheme].secondaryText,
     },
+    saveButton: {
+      backgroundColor: Colors[scheme].tint,
+      borderRadius: 10,
+      height: 45,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 15,
+      paddingHorizontal: 25,
+    },
+    saveButtonText: {
+      color: Colors[scheme].textOnTint,
+      fontSize: 16,
+      marginLeft: 5,
+      fontWeight: 'bold',
+    },
   });
 
   const onItemClick = (item: any) => {
     console.log(item)
+  }
+
+  const isListEmpty = () => {
+    let isEmpty = true;
+    recipeListItems.forEach((list: any) => {
+      if (list.data.length > 0) {
+        isEmpty = false;
+      }
+    });
+
+    return isEmpty;
   }
 
   const Item = ({ item }: { item: any }) => {
@@ -171,16 +210,44 @@ export default function Recipes({ navigation }: RootTabScreenProps<'Recipes'>) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <SectionList
-        renderItem={({ item }) =>
-          <Item item={item} />}
-        sections={recipeListItems}
-        keyExtractor={(item, index) => item + index}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text style={styles.header}>{title}</Text>
-        )}
-        stickySectionHeadersEnabled={false}
-      />
+      {isListEmpty()
+        ?
+        <View style={styles.emptyScreen}>
+          <MaterialIcons
+            name="fastfood"
+            size={120}
+            color={Colors[scheme].border}
+          />
+          <Text style={styles.emptyScreenText}>
+            Noch keine Rezepte gespeichert
+          </Text>
+          <Pressable
+            onPress={() => navigation.navigate('ModalNewRecipe')}
+            style={({ pressed }) => ({
+              opacity: pressed ? 0.5 : 1
+            })}>
+            <View style={styles.saveButton}>
+              <MaterialIcons
+                name="add"
+                size={26}
+                color={Colors[scheme].textOnTint}
+              />
+              <Text style={styles.saveButtonText}>Neues Rezept</Text>
+            </View>
+          </Pressable>
+        </View>
+        :
+        <SectionList
+          renderItem={({ item }) =>
+            <Item item={item} />}
+          sections={recipeListItems}
+          keyExtractor={(item, index) => item + index}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
+          )}
+          stickySectionHeadersEnabled={false}
+        />
+      }
     </SafeAreaView>
   );
 }
