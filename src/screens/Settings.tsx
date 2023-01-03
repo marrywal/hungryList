f16043aa78e5a2276c968ab146ba839b0467b426
Maria Walzl <maria.walzl@mayflower.de>
2023-01-03 14:47:08 +0100

    remove unused demo files

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../../types';
 
@@ -9,7 +8,7 @@ export default function Settings({ navigation }: RootTabScreenProps<'Settings'>)
     <View style={styles.container}>
       <Text style={styles.title}>Einstellungen</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/Settings.tsx" />
+      
     </View>
   );
 }
