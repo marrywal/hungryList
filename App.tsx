6bcbcb59602c08ebbdae04170ef22fa4a399812c
Maria Walzl <maria.walzl@mayflower.de>
2023-01-05 16:23:15 +0100

    remove wrong import

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './src/hooks/useCachedResources';
